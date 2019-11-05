6d06986096e3f45091cd8641d9294cc3655da6b4
FelixDaniel7 <danielfernandesdk27@gmail.com>
2019-11-05 12:17:25 -0300

    A

--- database/ddlBrinquedosFesta.sql
+++ database/ddlBrinquedosFesta.sql
@@ -10,7 +10,7 @@ USE BrinquedosFesta; -- usar o bd BRINQUEDOSFESTA
 /* Criação da tabela CLIENTE / Cadastro do cliente */
 CREATE TABLE Cliente(
 	CodCliente INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    CPF CHAR(14),/*opcinal para o cliente */
+    CPF CHAR(14) NOT NULL,/*opcinal para o cliente */
     Nome VARCHAR(50),
     Telefone VARCHAR(11),
     Endereco VARCHAR(200),
@@ -20,9 +20,6 @@ CREATE TABLE Cliente(
     Senha VARCHAR(40),/* 40 por causa da criptografia MD5 e sha1*/
     NivelAcesso INT(1) 
 );
-/* Deixar o campo Email UNICO */
--- CREATE UNIQUE INDEX IDX_Email ON Cliente(Email);
-
 CREATE TABLE Administrador(
 	CodAdministrador INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     Nome VARCHAR(50),
@@ -32,7 +29,6 @@ CREATE TABLE Administrador(
 	Senha VARCHAR(40),
     NivelAcesso INT(1) 
 );
-
 /* Visivel apenas para o ADMINISTRADOR */
 CREATE TABLE Supervisor(
 	CodSupervisor INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -47,10 +43,6 @@ CREATE TABLE Supervisor(
     Senha VARCHAR(40),
     NivelAcesso INT(1) 
 );
-
- 
-
-
 /* Criação da tabela EQUIPAMENTO Tudo relacionado com o EQUIPAMENTO */
 CREATE TABLE Equipamento(
 	CodEquipamento INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -73,61 +65,55 @@ CREATE TABLE DatasDisponivel(
     CONSTRAINT FK_Equipamento_Datas FOREIGN KEY (CodEquipamento)
     REFERENCES Equipamento(CodEquipamento)
 );
--- Criação de um índice no campo DataDisponivel para chave estrangeira
--- CREATE INDEX IDX_DataDisponivel ON Datas(DataDisponivel);
-    
-    
-/* Visivel para o cliente*/
-CREATE TABLE Frete(
-    CodFrete INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    CodEquipamento INT NOT NULL,
-    CepDestino CHAR(8),
-    EnderecoMontagem VARCHAR(200),
-    
-     CONSTRAINT FK_Equipamentos_Frete FOREIGN KEY (CodEquipamento)
-		REFERENCES Equipamento (CodEquipamento)
-);
-   
-
 
 /* Visivel para o CLIENTE e para o ADMINISTRADOR */
 CREATE TABLE Supervisao(
 	CodSupervisao INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-    TipoSupervisao BIT, -- 0 sem supervisao , 1 com supervisao  
 	ValorSupervisao DECIMAL(8,2),
     CodSupervisor INT NOT NULL,
     
     CONSTRAINT FK_Supervisor_Supervisao FOREIGN KEY (CodSupervisor)
 		REFERENCES Supervisor (CodSupervisor),
         
+	-- em qual brinquedos foi feita a supervisao
 	CodEquipamento INT NOT NULL, 
-    
     CONSTRAINT FK_Equipamentos_Supervisao FOREIGN KEY (CodEquipamento)
 		REFERENCES Equipamento (CodEquipamento)
 );
+
+
 
 /* Visivel para o CLIENTE e para o ADMINISTRADOR */
 CREATE TABLE Pedido(
 	CodPedido INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     CodCliente INT NOT NULL,
-	CodItem INT NOT NULL,
-    CodSupervisao INT NULL,
+    EnderecoMontagem VARCHAR(200),
     DataPedido DATETIME, -- hora do envio do pedido 
     Data_de_uso DATE, -- 1970-12-31
     HorasAlugado DOUBLE,  -- Quantidade de horas de aluguel,aluguel cobrado por hora
     Data_Hora_Montagem DATETIME, -- 1970-01-01 00:00:00
-    Data_Hora_Desmontagem TIME,-- fazer o calculo horamontagem mais horas usadas
-    
-    PrecoFinal DECIMAL(8,2),
+    -- Data_Hora_Desmontagem TIME,-- fazer o calculo hora montagem mais horas usadas 
+    --  não precisa pq é calculavel
+    PrecoFinal DECIMAL(8,2),-- preço com o frete
     FormaPagamento VARCHAR(20),
     
-    CONSTRAINT FK_Cliente_Aluguel FOREIGN KEY (CodCliente)
+    CONSTRAINT FK_Cliente_Pedido FOREIGN KEY (CodCliente)
 		REFERENCES Cliente(CodCliente),
-    
-    CONSTRAINT FK_Equipamento_Aluguel FOREIGN KEY (CodEquipamento)
-		REFERENCES Equipamento(CodEquipamento),
-    
-    CONSTRAINT FK_Supervisao_Aluguel FOREIGN KEY (CodSupervisao)
-		REFERENCES Supervisao(CodSupervisao)
+        
+	CONSTRAINT FK_Frete_Pedido FOREIGN KEY (CodCliente)
+		REFERENCES Cliente(CodCliente)
 ); 
 
+CREATE TABLE Itens(
+	CodItem INT NOT NULL PRIMARY KEY auto_increment,
+    CodPedido INT NOT NULL,
+    CodEquipamento INT NOT NULL,
+    Preco DECIMAL(8,2), -- preço do equipamento novamente, campo a ser preenchido depois / não vai pegar esse preço do banco de dados
+    
+    
+	CONSTRAINT FK_Pedido_Itens FOREIGN KEY (CodPedido) 
+		REFERENCES Pedido(CodPedido),
+ 
+	CONSTRAINT FK_Equipamento_Itens FOREIGN KEY (CodEquipamento) 
+		REFERENCES Equipamento(CodEquipamento)
+);
