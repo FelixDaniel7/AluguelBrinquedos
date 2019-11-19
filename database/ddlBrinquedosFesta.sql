0769f49f8e3c73df60e8cf1e0e86eb4f1d05c121
FelixDaniel7 <danielfernandesdk27@gmail.com>
2019-11-19 18:00:58 -0300

    banco de dados

---
+++ database/ddlBrinquedosFesta.sql
@@ -0,0 +1,91 @@
+USE mysql;
+
+DROP DATABASE BrinquedosFesta;
+
+CREATE DATABASE BrinquedosFesta CHARACTER SET UTF8 COLLATE=utf8_unicode_ci; /* criação do BD */
+
+USE BrinquedosFesta; -- usar o bd BRINQUEDOSFESTA
+
+/*Tabelas de usuarios são separadas por terem diferentes dados armazenados*/
+/* Criação da tabela CLIENTE / Cadastro do cliente */
+CREATE TABLE Cliente(
+	CodCliente SMALLINT NOT NULL PRIMARY KEY AUTO_INCREMENT, -- TINYINT menos q Smallint e int
+    CPF CHAR(11) NOT NULL,/*opcinal para o cliente */
+    Nome VARCHAR(50),
+    Telefone VARCHAR(10),
+    Celular CHAR(11),
+    
+    CEP CHAR(8),
+    Endereco VARCHAR(200),
+    Numero VARCHAR(5),
+    Bairro VARCHAR(50),
+    Complemento VARCHAR(50)
+);
+
+-- para admin, supervisor e cliente
+CREATE TABLE Usuario(
+	CodUsuario SMALLINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    Nome VARCHAR(50),
+    Email VARCHAR(50) unique,-- para recuperar senha
+    Login VARCHAR(25),
+    Senha VARCHAR(40),/*para criptografia*/
+    Tipo ENUM('super','Administrador','Moderador') -- moderador pode fazer os pedidos
+);
+
+/* Criação da tabela EQUIPAMENTO Tudo relacionado com o EQUIPAMENTO */
+CREATE TABLE Equipamento(
+	CodEquipamento SMALLINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Nome VARCHAR(20),
+    Descricao VARCHAR(100),
+    Peso DECIMAL(7,2),/*em KG*/
+    Altura DECIMAL(7,2),/*em Metros*/
+    Comprimento DECIMAL(7,2),/*em Metros*/
+    Largura DECIMAL(7,2),/*em Metros*/
+    Preco DECIMAL(8,2)
+);	
+    
+-- Normalização, criação da tabela DATAS
+CREATE TABLE DatasDisponivel(
+	DataInicial DATETIME NOT NULL,
+    DataFinal DATETIME NOT NULL,
+	
+    CodEquipamento SMALLINT NOT NULL, -- pesquisar normalização
+    
+    CONSTRAINT FK_Equipamento_Datas FOREIGN KEY (CodEquipamento)
+    REFERENCES Equipamento(CodEquipamento)
+);
+
+/* Visivel para o CLIENTE e para o ADMINISTRADOR */
+CREATE TABLE Pedido(
+	CodPedido INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    CodCliente SMALLINT NOT NULL,
+    CodUsuario SMALLINT NOT NULL,-- para caso alguem faça um pedido
+    EnderecoMontagem VARCHAR(200),
+    DataPedido DATETIME, -- hora do envio do pedido 
+    Data_de_uso DATE, -- 1970-12-31
+    HorasAlugado DOUBLE,  -- Quantidade de horas de aluguel,aluguel cobrado por hora
+    Data_Hora_Montagem DATETIME, -- 1970-01-01 00:00:00
+    -- Data_Hora_Desmontagem TIME,-- fazer o calculo hora montagem mais horas usadas 
+    --  não precisa pq é calculavel
+    PrecoFinal DECIMAL(8,2),-- preço com o frete
+    FormaPagamento VARCHAR(20),
+    
+    Supervisao BIT,-- se tem supervisor adiciona tanto no preço
+    
+    CONSTRAINT FK_Cliente_Pedido FOREIGN KEY (CodCliente)
+		REFERENCES Cliente(CodCliente)
+); 
+
+CREATE TABLE Itens(
+	CodItem SMALLINT NOT NULL PRIMARY KEY auto_increment,
+    CodPedido INT NOT NULL,
+    CodEquipamento SMALLINT NOT NULL,
+    Preco DECIMAL(8,2), -- preço do equipamento novamente, campo a ser preenchido depois / não vai pegar esse preço do banco de dados
+    
+    
+	CONSTRAINT FK_Pedido_Itens FOREIGN KEY (CodPedido) 
+		REFERENCES Pedido(CodPedido),
+ 
+	CONSTRAINT FK_Equipamento_Itens FOREIGN KEY (CodEquipamento) 
+		REFERENCES Equipamento(CodEquipamento)
+);
---
+++ database/dmlInsert_BrinquedosFesta.sql
@@ -0,0 +1,63 @@
+USE BrinquedosFesta;
+
+INSERT INTO Cliente(CPF,Nome,Telefone,Email,Senha,Endereco)
+	VALUES ('12345678912','Rodolfo da Silva Santos','11976753358',
+			'rodolfo.santos@gmail.com','abc1234567','Rua das rosas, 485-Jardim çççãaaáááá´´aããã  ??Nemus');
+INSERT INTO Cliente (CPF,Nome,Telefone,Email,Senha,Endereco)
+	VALUES ('98745621365','Robersval Silveira Pinheiro','11963236521',
+			'robersval1985_silveira@hotmail','jhf1234567','Rua 10, 265-Jd.Represa');
+            
+           INSERT INTO  Administrador(Nome,Email,Login,Senha,NivelAcesso)
+           Values ('blabla', 'daniel@gmail.com','blaba213','123',1);
+           
+           INSERT INTO  Administrador(Nome,Email,Login,Senha,NivelAcesso)
+           Values ('blabla2', 'daniel@gmail.com','blaba3658','123456',1);
+           
+           select * from usuario;
+           
+           UPDATE Usuario 
+                        SET Senha = '123'
+                         WHERE Email = 'danielfernandesdk27@gmail.com';
+           
+           SELECT * FROM Usuario 
+                    WHERE Login = 'Daniel' AND Senha = 'a3dd91d922fcff42f64ac37e9140b02a00e4ce01' AND Tipo = 'Administrador' 
+           
+insert into usuario (Tipo)
+values ('administradorghff');
+	
+INSERT INTO Equipamento (Nome,Descricao,Preco) 
+	VALUES ('Piscina de Bolinhas','100m X 200m',80.00);
+INSERT INTO Equipamento (Nome,Descricao,Preco) 
+	VALUES ('Cama elástica','165m X 298m Material Super Elastico',120.00);
+INSERT INTO Equipamento (Nome,Descricao,Preco) 
+	VALUES ('Castelo inflável','138m X 120m Anti-furo',150.00);
+INSERT INTO Equipamento (Nome,Descricao,Preco) 
+	VALUES ('Alogão Doce','3 velocidades',100.00);
+    INSERT INTO Equipamento (Nome,Descricao,Preco) 
+	VALUES ('blaaaaáaaãaaaa','3 velocidades',15500.00);
+    
+INSERT INTO Datas (DataDisponivel,CodEquipamento)
+	VALUES ('2019-08-06',4);
+    
+select * from Datas;
+
+Select nome from equipamento where nome LIKE '%cama%';
+
+SELECT E.Nome , E.Preco , D.DataDisponivel 
+FROM Equipamento AS E INNER JOIN Datas AS D 
+ON E.CodEquipamento = D.CodEquipamento; 
+SELECT * FROM Cliente WHERE Nome LIKE 'Rodolfo%';
+
+INSERT INTO Supervisao(CodSupervisao,TipoSupervisao,ValorSupervisao)
+VALUES(0,0);
+
+select * from Equipamento;
+    
+INSERT INTO Aluguel (CodCliente,CodEquipamento,DataAluguel,Data_de_uso,
+					HorasAlugado,DataMontagem,DataDesmontagem,EnderecoMontagem,
+                    Supervisao,PrecoFinal,FormaPagamento) 
+	VALUES (1,4,'2019-07-30 02:30:55','2019-08-05',
+			3.5,'2019-08-05 14:00:00','2019-08-05 17:00:00','Rua das rosas, 485-Jardim Nemus',
+            0,556.47,'Cartão');
+select * from Aluguel;
+    
