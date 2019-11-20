75bf326c5c4d96bf50c51aa58224731228af6127
Pedro0308 <55712036+Pedro0308@users.noreply.github.com>
2019-11-19 23:12:31 -0200

    o

--- index.php
+++ index.php
@@ -23,9 +23,13 @@
 </head>
 
 <body style="background-color: cornflowerblue; ">
+
   <!-- Navigation -->
-  
-  include_once(menu/menu_superior.php);
+  <?php
+
+  include_once ("view/menu/menu-superior.php");
+
+  ?>
 
   <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
@@ -150,44 +154,9 @@
     </div>
   </section>
   <!-- Footer -->
-  <footer class="footer bg-light">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-6 h-100 text-center text-lg-left my-auto">
-          <ul class="list-inline mb-2">
-            <li class="list-inline-item">
-              <a href="#" >Sobre</a>
-            </li>
-            <li class="list-inline-item">&sdot;</li>
-            <li class="list-inline-item">
-              <a href="#">Contatos</a>
-            </li>
-           
-          </ul>
-          <p class="text-muted small mb-4 mb-lg-0">&copy; Aluguel Brinquedos 2019.</p>
-        </div>
-        <div class="col-lg-6 h-100 text-center text-lg-right my-auto">
-          <ul class="list-inline mb-0">
-            <li class="list-inline-item mr-3">
-              <a href="#">
-                <i class="fab fa-facebook fa-2x fa-fw"></i>
-              </a>
-            </li>
-            <li class="list-inline-item mr-3">
-              <a href="#">
-                <i class="fab fa-twitter-square fa-2x fa-fw"></i>
-              </a>
-            </li>
-            <li class="list-inline-item">
-              <a href="#">
-                <i class="fab fa-instagram fa-2x fa-fw"></i>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </footer>
+  <?php 
+  include "menu/menu-inferior.php";
+  ?>
 
   <!-- Bootstrap core JavaScript -->
   <script src="vendor/jquery/jquery.min.js"></script>
