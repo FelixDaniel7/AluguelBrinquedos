3d7c657c7df3fbf18ebada37d224327b52377441
Pedro0308 <55712036+Pedro0308@users.noreply.github.com>
2019-11-19 22:45:18 -0200

    legal
    legal

--- Tcc/index.php
+++ Tcc/index.php
@@ -24,68 +24,8 @@
 
 <body style="background-color: cornflowerblue; ">
   <!-- Navigation -->
-  <nav class="nav justify-content-center bg-light">
-    <ul class="nav justify-content-center">
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#">Contatos: (11)95555-5555</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#">Contatos: (11)4444-4444</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#">E-mail: nome@gmail.com</a>
-        </li>
-    </ul>
-  </nav>
   
-  <nav class="navbar navbar-light bg-light static-top"> &nbsp;
-    <p>&nbsp;
-      <a href="index.php">
-        <img src="img/Logo/logo1.png" alt="logo img"  width="300" height="51">
-    </p>
-    <div class="col-lg-6 h-100 text-center text-lg-right my-auto"> 
-        <ul class="list-inline mb-0">
-          <li class="list-inline-item mr-3"> 
-            </a>
-          </li>
-        </ul>
-    </div>
-  </nav>
-  
-
-<div class="container" >
-  <nav class="navbar navbar-expand-lg navbar-dark">
-      <a class="navbar-brand" href="#">Home</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-    <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item dropdown">
-              <a class="navbar-brand  dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Brinquedos
-              </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <a class="dropdown-item" href="#">Ação</a>
-                  <a class="dropdown-item" href="#">Outra ação</a>
-                  <div class="dropdown-divider"></div>
-                  <a class="dropdown-item" href="#">Algo mais aqui</a>
-                </div>
-              </li>
-              <li class="nav-item active">
-                <a class="navbar-brand" href="#">Galeria</a>
-              </li>
-              <li class="nav-item">
-                  <a class="navbar-brand" href="#">Sobre</a>
-              </li>
-    </div>
-              <li class="nav-item">
-                  <a class="navbar-brand fas fa-shopping-cart fa-2x fa-fw carrinho"  data-toggle="modal" data-target="#modalCarrinho"></a>
-              </li>
-          </ul>
-            </a>
-   </nav>
-</div>
+  include_once(menu/menu_superior.php);
 
   <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
