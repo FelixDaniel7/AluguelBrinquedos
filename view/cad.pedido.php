8a8b32c1c7015fa2fdc3c8f2ca2af08e683587c8
FelixDaniel7 <danielfernandesdk27@gmail.com>
2019-11-21 18:03:50 -0300

    form pedido

--- view/cad.pedido.php
+++ view/cad.pedido.php
@@ -23,133 +23,107 @@ include_once('../controller/pedido.controller.php'); ?>
     <script src="../ajax/pedido.ajax.js"></script>
     <!-- Alertas Bonitinhos -->
     <script src="../js/sweetalert.js"></script>
-    <!-- datatable -->
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
 </head>
 
-    
 
-  <section class="features-icons bg-light text-center" >
-
-      <div class="container">
-          <h3>Informações Pessoais</h3>
-          <hr>
-          <form>
-
-              <div class="form-row">
-                  <div class="form-group col-md-6">
-                    <label for="Text">Nome <span>*</span></label>
-                    <input type="text" class="form-control" id="Nome" placeholder="Alfredo">
-                  </div>                
-                </div>
-                <div class="form-row">
-                    <div class="form-group col-md-6">
-                      <label for="Text">Celular <span>*</span></label>
-                      <input type="number" class="form-control" id="Telefone" placeholder="(11)9999-9999">
-                    </div>                    
-                  </div>
-              <div class="form-row">
-                <div class="form-group col-md-6">
-                  <label for="inputEmail4">Email <span>*</span></label>
-                  <input type="email" class="form-control" id="inputEmail4" placeholder="Email" >
-                </div>               
-              </div>
-
-              <div class="form-row">
-                  <div class="form-group col-md-6">
-                      <label for="inputCPF">CPF <span>*</span></label>
-                      <input type="text" class="form-control" id="inputCEP">
-                    </div>
-             </div>
+<div class="container bg-light text-center ">
+<form name="form_cad_cliente_pedido" method="POST">    
 
 
-    <h3>Local da Montagem</h3>
-    <hr>
-
-        <div class="form-row">
-            <div class="form-group col-md-6">
-              <label for="Text"> CEP <span>*</span></label>
-              <input type="number" class="form-control" id="Nome" placeholder="Informe seu CEP">
-            </div>                
-          </div>
-          <div class="form-row">
-              <div class="form-group col-md-6">
-                <label for="Text">Endereço <span>*</span></label>
-                <input-group-text type="text" class="form-control" id="Endereço" placeholder="Rua da Florinda">
-              </div>                    
-            </div>
-        <div class="form-row">
-          <div class="form-group col-md-6">
-            <label for="inputEmail4">Bairro <span>*</span></label>
-            <input-group-text type="text" class="form-control" id="Bairro" placeholder="Vila do chaves">
-          </div>               
-        </div>
-
-        <div class="form-row">
-            <div class="form-group col-md-6">
-                <label for="inputCPF">Numero</label> <span>*</span></label>
-                <input type="number" class="form-control" id="Numero" placeholder="7" >
-              </div>
-       </div>
-       <div class="form-row">
-        <div class="form-group col-md-6">
-          <label for="inputEmail4">Complemento <span>*</span></label>
-          <input type="text" class="form-control" id="Complemento" placeholder="Do lado do barril">
-        </div>               
-      </div>
-
-      
-    <h3>Dados do Pedido</h3>
-    <hr>
-
-        <div class="form-row">
-            <div class="form-group col-md-6">
-              <label for="Text"> Data de uso <span>*</span></label>
-              <input type="date" class="form-control" id="dataUso" pattern="[0-9]{2}\/[0-9]{2}\/[0-9]{4}$" min="2019-11-07" max="2022-12-31" >
-            </div>                
-          </div>
-          <div class="form-row">
-              <div class="form-group col-md-6">
-                <label for="Text">Horas de utilização <span>*</span></label>
-                <input type="number" class="form-control" placeholder="Horas" maxlength="2" >
-              </div>                    
-            </div>
-        <div class="form-row">
-          <div class="form-group col-md-6">
-            <label for="inputEmail4">Horário de Montagem <span>*</span></label>
-            <input type="time" class="form-control" id="hrMontagem" >
-          </div>               
-        </div>
-
-        <div class="form-row">
-            <div class="form-group col-md-6">
-              <label for="Text">Forma de pagamento<span>*</span></label>
-              <select class="custom-select" id="inputGroupSelect01" >
-                    <option selected>Escolha...</option>
-                    <option value="1">Boleto</option>
-                    <option value="2">Cartão</option>
-                    </select>
-            </div>
-          </div>
-
-      <div class="form-group">
-          <div class="form-check">
-            <input class="form-check-input" type="checkbox" id="gridCheck" >
-            <label class="form-check-label" for="gridCheck">
-              Concordo com os termos
-            </label>
-          </div>
-        </div>
-        <button type="submit" class="btn btn-primary">Entrar</button>
-      </form>
-              </div>
-            </div>
-          </section>
+<section id="pessoais">
   
 
+    <h3>Informações Pessoais</h3>
+    <label for="Text">Nome <span>*</span></label>
+    <input type="text" class="form-control" name="txtnome" placeholder="Alfredo">
 
+    <label for="Text">Celular <span>*</span></label>
+    <input type="number" class="form-control" name="txtcelular" placeholder="(11)9999-9999">
+
+    <label for="inputEmail4">Email <span>*</span></label>
+    <input type="email" class="form-control" name="txtemail" placeholder="Email" >
+
+    <label for="inputCPF">CPF <span>*</span></label>
+    <input type="text" class="form-control" name="txtcpf">
+
+    <button type="button" id="proximo">Proximo</button>
+
+</section>
+
+<section id="montagem" style="display: none">
+
+    <h3>Local da Montagem</h3>
+
+    <label for="Text"> CEP <span>*</span></label>
+    <input type="number" class="form-control" name="txtcep" placeholder="Informe seu CEP">
+
+    <label for="Text">Endereço <span>*</span></label>
+    <input type="text" class="form-control" name="txtendereco" placeholder="Rua da Florinda">
+
+    <label for="inputEmail4">Bairro <span>*</span></label>
+    <input type="text" class="form-control" name="txtbairro" placeholder="Vila do chaves">
+
+
+    <label for="inputCPF">Numero</label> <span>*</span></label>
+    <input type="number" class="form-control" name="txtnumero" placeholder="725" >
+
+    <label for="inputEmail4">Complemento <span>*</span></label>
+    <input type="text" class="form-control" name="txtcomplemento" placeholder="Do lado do barril">
+
+    
+    <button type="button" id="anterior">Anterior</button>
+    <button type="button" id="proximo">Proximo</button>
+
+</section>
+
+<section id="pedido" style="display: none">
+
+    <h3>Dados do Pedido</h3>
+
+
+
+    <label for="Text"> Data de uso <span>*</span></label>
+    <input type="date" class="form-control" name="txtdataUso" pattern="[0-9]{2}\/[0-9]{2}\/[0-9]{4}$" min="2019-11-07" max="2022-12-31" >
+
+
+    <label for="Text">Horas de utilização <span>*</span></label>
+    <input type="number" class="form-control" placeholder="txthorasAlugado" maxlength="2" >
+
+    <label for="inputEmail4">Horário de Montagem <span>*</span></label>
+    <input type="time" class="form-control" name="txthoraMontagem" >
+
+
+
+    <label for="Text">Forma de pagamento<span>*</span></label>
+    <select class="custom-select" name="txtformaPagamento" >
+      <option selected>Escolha...</option>
+      <option value="Dinheiro">Dinheiro</option>
+      <option value="Cartão">Cartão</option>
+    </select>
+
+    <label for="Text">Supervisão<span>*</span></label>
+    <select class="custom-select" name="txtsupervisao" >
+    <option value="0">Sem Supervisão</option>
+      <option value="1">Com Supervisão</option>
+    </select>
+
+    <h4>Brinquedos escolhidos</h4>
+    <input type="checkbox" name="txtpulapula" id="">Pula Pula <br>
+    <input type="checkbox" name="txtcastelo" id="">Castelo <br>
+
+
+    <button type="button" id="anterior">Anterior</button>
+
+    <button type="submit" class="btn btn-primary">Enviar</button>
+    
+</section>
+
+<br>
+
+    
+</form>
+</div>
 </body>
-
 </html>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
