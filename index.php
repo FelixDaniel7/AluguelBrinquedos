1cd63c1665df898a1876c88ec3a7f5b6e0fc5405
Pedro0308 <55712036+Pedro0308@users.noreply.github.com>
2019-11-22 20:41:42 -0200

    juntando tudo

---
+++ index.php
@@ -0,0 +1,283 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <title>Home - Aluguel Briquedos</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="view/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="view/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="view/vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+  <!-- Custom styles for this template -->
+  <link href="view/css/landing-page.min.css" rel="stylesheet">
+  <link href="view/css/css.css" rel="stylesheet">
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="view/vendor/jquery/jquery.min.js"></script>
+  <script src="view/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body class="fundo">
+
+  <!-- Navigation -->
+  <nav class="nav justify-content-center bg-light">
+    <ul class="nav justify-content-center">
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Contatos: (11)95555-5555</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Contatos: (11)4444-4444</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">E-mail: nome@gmail.com</a>
+        </li>
+    </ul>
+  </nav>
+  
+  <nav class="navbar navbar-light bg-light static-top"> &nbsp;
+    <p>&nbsp;
+      <a href="index.php">
+        <img src="view/img/Logo/logo1.png" alt="logo img"  width="300" height="51">
+    </p>
+    <div class="col-lg-6 h-100 text-center text-lg-right my-auto"> 
+        <ul class="list-inline mb-0">
+          <li class="list-inline-item mr-3"> 
+            </a>
+          </li>
+          <li class="nav-item">
+                  <a class="navbar-brand fas fa-shopping-cart fa-2x fa-fw perfil"  data-toggle="modal" data-target="#modalCarrinho"></a>
+              </li>
+        </ul>
+    </div>
+  </nav>
+  
+
+<div class="container" >
+  <nav class="navbar navbar-expand-lg navbar-dark">
+      <a class="navbar-brand" href="index.php">Home</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+    <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item dropdown">
+              <a class="navbar-brand  dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Brinquedos
+              </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item " href="view/Cama_Elastica.php">Cama Elástica</a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="view/Piscina_Bolinhas.php">Piscina De Bolinhas</a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="view/Castelo_Inflavel.php">Castelo Inflável</a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="view/Maquina_Algodao.php">Máquina De Algodão Doce‎</a>
+              </li>
+              <li class="nav-item active">
+                <a class="navbar-brand" href="#">Galeria</a>
+              </li>
+              <li class="nav-item">
+                  <a class="navbar-brand" href="#">Sobre</a>
+              </li>
+    </div>
+             
+          </ul>
+            </a>
+   </nav>
+</div>
+
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="view/img/Menu/castelo.jpg" alt="Primeiro Slide">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Brinquedoooooooooooooo</h5>
+            <p>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
+          <button type="button" class="btn btn-primary btn-lg">
+            Conferir
+          </button>
+        </div>
+    </div>
+  <div class="carousel-item">
+    <img class="d-block w-100" src="view/img/Menu/caminha.jpg" alt="Segundo Slide">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Brinquedoooooooooooooo</h5>
+          <p>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
+        <button type="button" class="btn btn-primary btn-lg">
+          Conferir
+        </button>
+      </div>
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="view/img/Menu/bolinha.jpg" alt="Terceiro Slide">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Brinquedoooooooooooooo</h5>
+          <p>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
+          <button type="button" class="btn btn-primary btn-lg">Conferir</button>
+        </div>
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Anterior</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Próximo</span>
+    </a>
+  </div>
+
+    <section class="features-icons bg-light text-center" >
+        <h5>Brinquedos</h5>
+        <hr>
+      <div class="container">
+        <div class="row">
+            <div class="card-deck">
+            <div class="card">
+                <img class="card-img-top" src="view/img/produtos/CamaElastica/1.jpg" alt="Imagem de capa do card">
+                <div class="card-body">
+                  <h5 class="card-title">Cama elástica</h5>
+                  <p class="card-text">A cama elástica é sempre o brinquedo mais querido e procurado nas festas e não há criança que não goste de passar horas pulando e brincando sem parar, por isso, é um brinquedo ideal para buffets, condomínios, clubes, casas e hotéis. </p>
+                </div>
+                <div class="card-footer">
+                <a href="Cama_Elastica.php" class="btn btn-primary btn-sm" tabindex="-1" role="button">Conferir</a>
+                </div>
+              </div>
+              <div class="card">
+                <img class="card-img-top" src="view/img/produtos/PiscinaBolinhas/1.jpg" alt="Imagem de capa do card">
+                <div class="card-body">
+                  <h5 class="card-title">Piscina de bolinhas</h5>
+                  <p class="card-text">Uma festa infantil não é uma festa sem uma piscina de bolinhas, ela é um item indispensável que vai encantar as crianças e deixar a festa muito mais divertida!</p>
+                </div>
+                <div class="card-footer">
+                <a href="Piscina_Bolinhas.php" class="btn btn-primary btn-sm" tabindex="-1" role="button">Conferir</a>
+                </div>
+              </div>
+              <div class="card">
+                <img class="card-img-top" src="view/img/produtos/MaquinaAlgodão/1.jpg" alt="Imagem de capa do card">
+                <div class="card-body">
+                  <h5 class="card-title">Máquina de Algodão Doce</h5>
+                  <p class="card-text">Uma Máquina de Algodão Doce é um acessório indispensável para deixar uma festinha infantil ainda mais divertida para as crianças, até mesmo para os adultos. Aqui na Magia Brinquedos nós temos modelos de alta qualidade, fácil manejo e transporte e com preços incríveis. Confira os nosso produtos!</p>
+                </div>
+                <div class="card-footer">
+                <a href="Castelo_Inflavel.php" class="btn btn-primary btn-sm" tabindex="-1" role="button">Conferir</a>
+                </div>
+              </div>
+              <div class="card">
+                <img class="card-img-top" src="view/img/produtos/CasteloInflavel/1.jpg" alt="Imagem de capa do card">
+                <div class="card-body">
+                  <h5 class="card-title">Castelo Infável</h5>
+                  <p class="card-text">Brinquedos infláveis fazem a alegria da criançada em qualquer festa infantil, são brinquedos que chamam muito a atenção e proporcionam horas de muita diversão.</p>
+                </div>
+                <div class="card-footer">
+                <a href="Castelo_Inflavel.php" class="btn btn-primary btn-sm" tabindex="-1" role="button">Conferir</a>
+                </div>
+              </div>
+              </div>
+        </div>
+      </div>
+    </section>
+
+
+  <!-- Icons Grid -->
+  <section class="features-icons bg-light text-center" style="background:url('view/img/Menu/bolinha.jpg')"">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4">
+          <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+            <div class="features-icons-icon d-flex">
+              <i class="icon-screen-desktop m-auto text-black"></i>
+            </div>
+            <h3>Praticidade</h3>
+            <p class="lead mb-0">Alugue seu brinquedo em apenas alguns cliques!</p>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+            <div class="features-icons-icon d-flex">
+              <i class="fa fa-truck m-auto text-black"></i>
+            </div>
+            <h3>Entrega rapida</h3>
+            <p class="lead mb-0">Entrega rápida e no dia agendado!</p>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="features-icons-item mx-auto mb-0 mb-lg-3">
+            <div class="features-icons-icon d-flex">
+              <i class="fas fa-lock m-auto text-black"></i>
+            </div>
+            <h3>Brinquedos Seguros</h3>
+            <p class="lead mb-0">Brinquedos revisados frequentemente para uma maior segurança!</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Footer -->
+  <?php 
+  include "view/menu/menu-inferior.php";
+  ?>
+
+
+  <div class="modal" id="modalCarrinho" tabindex="-1" role="dialog"aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Carrinho rápido</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <table class="table table-bordered text-light">
+              <thead class="text-center bg-primary  ">
+                <th>Imagem</th>
+                <th>Produto</th>
+                <th>Quantidade</th>
+                <th>Valor</th>
+                <th>Acão</th>
+              </thead>
+              <tbody>
+                <tr>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td> </td>
+                  <td class="text-center"><a href="" class="fas fa-ban"></a></td>
+                </tr>
+              </tbody>
+            </table>
+            <table class="table table-bordered">
+            <thead>
+                <th class="text-right">Total: R$ 00,00</th>  
+              </thead>
+              </table>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+            <button type="button" class="btn btn-primary">Finalizar pedido</button>
+          </div>
+        </div>
+      </div>
+  </div>
+</body>
+</html>
+
+
+
+
+
