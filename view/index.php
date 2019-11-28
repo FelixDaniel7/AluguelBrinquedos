eb5d76c99e04f6ede1f3e1a694be85ee61a2c0fb
FelixDaniel7 <53989205+FelixDaniel7@users.noreply.github.com>
2019-11-28 21:33:31 -0200

    index bonitinho

--- view/index.php
+++ view/index.php
@@ -1,6 +1,8 @@
 <?php 
 include_once("../controller/equipamento.controller.php");
 include_once("../controller/pedido.controller.php");
+
+$Aleatorio = rand(1,4);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -24,17 +26,12 @@ include_once("../controller/pedido.controller.php");
   <!-- Custom styles for this template -->
   <link href="css/landing-page.min.css" rel="stylesheet">
   <link href="css/css.css" rel="stylesheet">
-
-  
-
     <!-- Jquery -->
     <script src="../js/jquery-3.4.1.min.js"></script>
     <!-- Ajax -->
     <script src="../ajax/pedido.ajax.js"></script>
     <!-- Alertas Bonitinhos -->
     <script src="../js/sweetalert.js"></script>
-    
-
   <!-- Bootstrap core JavaScript -->
   <script src="vendor/jquery/jquery.min.js"></script>
   <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
@@ -53,29 +50,31 @@ include_once("../controller/pedido.controller.php");
     <div class="carousel-item active">
       <img class="d-block w-100" src="img/Menu/castelo.jpg" alt="Primeiro Slide">
         <div class="carousel-caption d-none d-md-block">
-          <h5>Brinquedoooooooooooooo</h5>
-            <p>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
-          <button type="button" class="btn btn-primary btn-lg">
-            Conferir
-          </button>
+          <h5>Praticidade</h5>
+            <h6><p>Alugue seu brinquedo em apenas alguns cliques!</p></h6>
+            <a href="Pag_Equipamento.php?CodEquipamento=<?php echo $Aleatorio;?>" class="btn btn-primary btn-lg" tabindex="-1" role="button">
+              Ver Mais
+            </a>
         </div>
     </div>
   <div class="carousel-item">
     <img class="d-block w-100" src="img/Menu/caminha.jpg" alt="Segundo Slide">
       <div class="carousel-caption d-none d-md-block">
-        <h5>Brinquedoooooooooooooo</h5>
-          <p>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
-        <button type="button" class="btn btn-primary btn-lg">
-          Conferir
-        </button>
+        <h5>Entrega rapida</h5>
+        <h6><p>Entrega rápida e no dia agendado!</p></h6>
+          <a href="Pag_Equipamento.php?CodEquipamento=<?php echo $Aleatorio;?>" class="btn btn-primary btn-lg" tabindex="-1" role="button">
+              Ver Mais
+            </a>
       </div>
       </div>
       <div class="carousel-item">
         <img class="d-block w-100" src="img/Menu/bolinha.jpg" alt="Terceiro Slide">
         <div class="carousel-caption d-none d-md-block">
-          <h5>Brinquedoooooooooooooo</h5>
-          <p>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
-          <button type="button" class="btn btn-primary btn-lg">Conferir</button>
+          <h5>Brinquedos Seguros</h5>
+          <h6><p>Brinquedos revisados frequentemente para uma maior segurança!</p></h6>
+          <a href="Pag_Equipamento.php?CodEquipamento=<?php echo $Aleatorio;?>" class="btn btn-primary btn-lg" tabindex="-1" role="button">
+              Ver Mais
+            </a>
         </div>
       </div>
     </div>
@@ -95,36 +94,48 @@ include_once("../controller/pedido.controller.php");
   <h2>Brinquedos</h2>
   <hr>
   <div class="container-fluid">
-    <div class="row"><!--essa-->
+    <div class="row">
       <div class="card-deck">
         <?php 
         if ($certo = $equi->ConsultarEquipamento()) {
           foreach($certo as $value){
           ?>
-          <div class="col-md-3" id="card-carrinho"><!-- essa -->
+          <div class="col-md-3" id="card-carrinho">
             <div class="card" style="height: 50rem;">
-              <img class="card-img-top" src="img/Produtos/<?php echo $value->Imagem;?>" alt="Imagem Equipamento">
+              <a href="Pag_Equipamento.php?CodEquipamento=<?php echo $value->CodEquipamento;?>" >
+                <img class="card-img-top" src="img/Produtos/<?php echo $value->Imagem;?>" alt="Imagem Equipamento">
+              </a>
               <div class="card-body">
-                <h5 class="card-title"><?php echo $value->Nome;?></h5>
-                <?php if ($value->Status != "Disponivel") {
-                  echo "<span class='badge badge-pill badge-danger'>Indisponivel</span>";
-                } else{
-                  echo "<span class='badge badge-pill badge-success'>Disponivel</span>";
-                }?>
-                <p class="card-text"><?php echo $value->Descricao;?></p>
-                
-              </div>
-              
-              <div class="card-footer">
-                <a href="Pag_Equipamento.php?CodEquipamento=<?php echo $value->CodEquipamento;?>" class="btn btn-primary btn-sm" tabindex="-1" role="button">Ver Mais</a>
-                
-                <?php if ($value->Status == 'Disponivel') {?>
-                  <hr>
-                  <button type='button' carrinho='btn_add_carrinho' value='<?php echo $value->CodEquipamento;?>' class='btn btn-warning btn-sm'>
-                  Adicionar ao carrinho
+                <div class="card-title">
+                  <?php if ($value->Status == 'Disponivel') {?>
+                    <button type='button' carrinho='btn_add_carrinho' value='<?php echo $value->CodEquipamento;?>' class='btn btn-warning btn-sm'>
+                        Adicionar ao carrinho
                     </button>
-                <?php }?>
+                    <hr>
+                    <a href="Pag_Equipamento.php?CodEquipamento=<?php echo $value->CodEquipamento;?>" >
+                      <span class='badge badge-pill badge-success'>Disponivel</span>
+                    </a>
+                    <br>
+                  <?php }else{ ?>
+                      <button type='button' class='btn btn-warning btn-sm' disabled>
+                          Adicionar ao carrinho
+                      </button>     
+                    <hr>
+                      <a href="Pag_Equipamento.php?CodEquipamento=<?php echo $value->CodEquipamento;?>" >
+                        <span class='badge badge-pill badge-danger'>Indisponivel</span>
+                      </a>
+                    <br>             
+                  <?php }?>
+                    <br>
+                  <h5><?php echo $value->Nome;?></h5>
+                </div>
+                <p class="card-text"><?php echo $value->Descricao;?></p>
+                <hr>
+                <a href="Pag_Equipamento.php?CodEquipamento=<?php echo $value->CodEquipamento;?>" class="btn btn-primary btn-sm" tabindex="-1" role="button">
+                  Ver Mais
+                </a>
               </div>
+              <div class="card-footer"></div>
             </div>
           </div>
           <?php }
