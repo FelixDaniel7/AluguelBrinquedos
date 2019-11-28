039aded58cfb62260f00c3947bec20848fb7d4dc
FelixDaniel7 <53989205+FelixDaniel7@users.noreply.github.com>
2019-11-27 22:00:14 -0200

    Update admin.equipamento.php

--- view/admin.equipamento.php
+++ view/admin.equipamento.php
@@ -19,7 +19,7 @@
     <link href="css/emoji-css.css" rel="stylesheet">
 
     <!-- datatable -->
-    <link href="css/jquery.dataTables.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
   </head>
   <body>
 
@@ -102,11 +102,8 @@
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="../js/popper.min.js"></script>
     <script src="../js/bootstrap.min.js"></script>
-
     <!-- datatable -->
     <script type="text/javascript" charset="utf8" src="../js/jquery.dataTables.min.js"></script>
-
-    
     <script>
     $('#tabela_equipamento').DataTable()
     </script>
