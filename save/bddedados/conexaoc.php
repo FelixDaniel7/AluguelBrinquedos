d6ec588dc3cc55dddcc40745f04bdb02f6079f30
FelixDaniel7 <danielfernandesdk27@gmail.com>
2019-11-29 16:51:58 -0300

    limpeza

--- save/bddedados/conexaoc.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$servidor = "localhost";
-	$usuario = "root";
-	$senha = "";
-	$dbname = "brinquedosfesta";
-	
-	//Criar a conexao
-	$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
