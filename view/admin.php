2a00582f6b385e567b7cca5fc269ba82af8ced95
FelixDaniel7 <danielfernandesdk27@gmail.com>
2019-11-29 17:15:23 -0300

    Quase lá
    falta arrumar o menu, tela de admin e o calculo do preço final

---
+++ view/admin.php
@@ -0,0 +1,79 @@
+<!-- vai estar na pagina de admin -->
+<?php 
+include_once('../controller/usuario.controller.php');
+
+session_start();
+
+$usu->Logado('administrador');
+
+//botao sair
+if (isset($_GET['logout']) && $_GET['logout'] == 'true') {
+    session_start();
+    //session_destroy();
+    unset($_SESSION['administrador']);
+    header("location: login.php");
+}
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Pagina do admin</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- Jquery -->
+    <script src="../js/jquery-3.4.1.min.js"></script>
+    <!-- Ajax -->
+    <script src="../ajax/usuario.ajax.js"></script>
+    <!-- Alertas Bonitinhos -->
+    <script src="../js/sweetalert.js"></script>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+ 
+  </head>
+  <body>
+
+  <nav class="navbar navbar-light bg-light">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="index.php"><span></span>Admin</a>
+      </div>
+    </div><!-- /.container-fluid -->
+  </nav>
+  <div class="row">
+    <div class="col-3">
+      <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+      <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a>
+      <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Perfil</a>
+      <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Mensagens</a>
+      <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Configurações</a>
+      </div>
+    </div>
+    <div class="col-9">
+      <div class="tab-content" id="v-pills-tabContent">
+      <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">1</div>
+      <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">2</div>
+      <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">3</div>
+      <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">4</div>
+      </div>
+  </div>
+  <div class="container">
+      <div>
+          <h2>Pagina admin</h2>
+          <a href="?logout=true" class="btn btn-danger">Sair</a>        
+      </div>
+  </div>
+  </body>
+</html>
+
+
+     
